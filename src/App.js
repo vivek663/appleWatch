135ca80aaee0d84fd013fe4a00b82c75a4c6f56a
Vivek <saivivektadikonda@gmail.com>
2024-12-24 08:48:48 +0530

    intial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoaderPage from "./Pages/LoaderPage";
+
+function App() {
+  return (
+    <div className="App">
+        <LoaderPage/>
+    </div>
+  );
+}
+
+export default App;
